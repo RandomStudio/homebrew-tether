4f7d7156fe6ccce5cbf63e052c0fd30ac3959a6c
stephen <stephen.buchanan@random.studio>
2023-07-03 13:11:40 +0200

    update formula version, too

--- Formula/tether.rb
+++ Formula/tether.rb
@@ -5,7 +5,7 @@ class Tether < Formula
   homepage "https://github.com/RandomStudio/homebrew-tether"
   url "https://github.com/RandomStudio/homebrew-tether/releases/download/v0.1.2/tether.tar.gz"
   sha256 "83c2e38e7a2da5bb60fb61b8d1db83529de89305515bde2cbeaca95e354ced1b"
-  version "0.1.1"
+  version "0.1.2"
 
   def install
     bin.install "tether"
