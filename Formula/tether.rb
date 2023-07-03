f28230f4ae7b75ab07a42a2e0bc851e4704242c5
stephen <stephen.buchanan@random.studio>
2023-07-03 15:13:23 +0200

    v0.1.3 formula

--- Formula/tether.rb
+++ Formula/tether.rb
@@ -3,9 +3,9 @@
 class Tether < Formula
   desc "Tether CLI utilities"
   homepage "https://github.com/RandomStudio/homebrew-tether"
-  url "https://github.com/RandomStudio/homebrew-tether/releases/download/v0.1.2/tether.tar.gz"
-  sha256 "83c2e38e7a2da5bb60fb61b8d1db83529de89305515bde2cbeaca95e354ced1b"
-  version "0.1.2"
+  url "https://github.com/RandomStudio/homebrew-tether/releases/download/v0.1.3/tether.tar.gz"
+  sha256 "ee9cc02aa13b9c3735d35bed6dd29f05de69f852623cf3cd56bf78222713db68"
+  version "0.1.3"
 
   def install
     bin.install "tether"
