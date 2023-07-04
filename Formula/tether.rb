dacfa06fda7206c44804ff5ef0b8de14d1f9a2b7
stephen <stephen.buchanan@random.studio>
2023-07-04 15:52:52 +0200

    v0.3.0 adds record

--- Formula/tether.rb
+++ Formula/tether.rb
@@ -3,9 +3,9 @@
 class Tether < Formula
   desc "Tether CLI utilities"
   homepage "https://github.com/RandomStudio/homebrew-tether"
-  url "https://github.com/RandomStudio/homebrew-tether/releases/download/v0.2.0/tether.tar.gz"
-  sha256 "639a351e33a285ea8a4feb1abfba21499361f00c93eecc573cbf366bbea2d23a"
-  version "0.2.0"
+  url "https://github.com/RandomStudio/homebrew-tether/releases/download/v0.3.0/tether.tar.gz"
+  sha256 "52806773b75008377548711f55ea02fe4e5026138c305dd8225c8807a53f6dac"
+  version "0.3.0"
 
   def install
     bin.install "tether"
